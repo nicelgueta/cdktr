99bcdc47271ff57192da693677d5fce1b6f94610
nicelgueta <nicelgueta@gmail.com>
2024-07-05 19:28:11 +0100

    refactored agent and principal rep servers into a single trait with default impl

--- src/server/msg.rs
+++ src/server/msg.rs
@@ -1,10 +1,11 @@
 
 use zeromq::ZmqMessage;
-
-#[derive(Debug)]
-pub struct ClientConversionError {
-    msg: String
-}
+use super::models::{
+    ClientResponseMessage,
+    ClientConversionError, 
+    traits::BaseClientRequestMessage
+};
+use async_trait::async_trait;
 
 impl ClientConversionError {
     pub fn new(msg: String) -> Self {
@@ -15,41 +16,28 @@ impl ClientConversionError {
     }
 }
 
-#[derive(PartialEq)]
-pub enum ClientResponseMessage {
-    InvalidMessageType,
-    Pong,
-    Success,
-    TMRestart
+pub enum AgentRequest{
+    Ping,
+    Heartbeat,
+    Reconnect
+}
+
+// TODO: make an extension of AgentRequest
+pub enum PrincipalRequest {
+    Ping,
 }
 
 impl Into<ZmqMessage> for ClientResponseMessage {
     fn into(self) -> ZmqMessage {
         let s = match self {
-            Self::InvalidMessageType => "InvalidRequest: Unrecognised message type",
-            Self::Pong => "PONG",
-            Self::Success => "SUCCESS",
-            Self::TMRestart => "TMRESTART"
+            Self::InvalidMessageType => "InvalidRequest: Unrecognised message type".to_string(),
+            Self::Pong => "PONG".to_string(),
+            Self::Success => "SUCCESS".to_string(),
+            Self::Heartbeat(pub_id) => format!("HEARTBEAT|{}", pub_id)
         };
         ZmqMessage::from(s)
     }
 }
-
-/// Remaining items
-/// 
-/// AGENT and PRINCIPAL
-/// GetTaskQueueSize
-/// GetMaxExecutors
-/// 
-///
-/// 
-/// PRINCIPAL ONLY
-/// TriggerTask((Task, AgentNodeId))
-/// AddFlow(FlowObj)
-/// RemoveFlow(id)
-/// ViewFlows
-/// 
-/// 
 
 fn parse_zmq_str(s: &str) -> (&str, Vec<String>) {
     let parsed_s: Vec<&str> = s.split("|").collect();
@@ -60,21 +48,15 @@ fn parse_zmq_str(s: &str) -> (&str, Vec<String>) {
         .collect();
     (msg_type, args)
 }
-trait BaseClientRequestMessage<T>: TryFrom<ZmqMessage> {
-    fn from_zmq_str(s: &str) -> Result<T, ClientConversionError> ;
-}
 
-pub enum AgentRequest{
-    Ping,
-    TMRestart,
-}
-impl BaseClientRequestMessage<AgentRequest> for AgentRequest {
+#[async_trait]
+impl BaseClientRequestMessage for AgentRequest {
     fn from_zmq_str(s: &str) -> Result<AgentRequest, ClientConversionError> {
         let (msg_type, args) = parse_zmq_str(s);
         match msg_type {
             // "GET_TASKS" => Ok(Self::GetTasks),
             "PING" => Ok(Self::Ping),
-            "TMRESTART" => Ok(Self::TMRestart),
+            "RECONNECT" => Ok(Self::Reconnect),
             _ => Err(ClientConversionError::new(format!("Unrecognised server message: {}", msg_type)))
         }
     }
@@ -93,10 +75,8 @@ impl TryFrom<ZmqMessage> for AgentRequest {
 
 // principal
 
-pub enum PrincipalRequest {
-    Ping,
-}
-impl BaseClientRequestMessage<PrincipalRequest> for PrincipalRequest {
+#[async_trait]
+impl BaseClientRequestMessage for PrincipalRequest {
     fn from_zmq_str(s: &str) -> Result<PrincipalRequest, ClientConversionError> {
         let (msg_type, args) = parse_zmq_str(s);
         match msg_type {
